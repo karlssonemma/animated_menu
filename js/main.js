224631bae76a8190d96dd03ac0064a9cbda94205
karlssonemma <karlssonemma93@gmail.com>
2020-11-07 16:54:17 +0100

    animated logo. changed animations.

--- js/main.js
+++ js/main.js
@@ -26,25 +26,32 @@ function moveNav() {
     let navLinks = document.querySelectorAll('.head__nav a');
     let logo = document.querySelector('.head__logo');
     
+    // TO ONLY BE RUN IF SCREEN-WIDTH IS MORE THAN 700px
+    if (window.screen.width > 700) {
+    
+        if (window.scrollY > 1) {
+            nav.classList.add('move-nav');
+            logo.classList.add('move-logo');
+            logo.style.animation = 'logoFade 2s cubic-bezier(.11,.49,0,1.72) forwards .5s'
+    
+            navLinks.forEach((link, index) => {
+                link.style.opacity = 0;
+                link.style.animation = `linkFade 1s cubic-bezier(.11,.49,0,1.72) ${index / 2 + 2}s 1 forwards`
+            });
+    
+        } else if (window.scrollY < 1) {
+            nav.classList.remove('move-nav');
+            logo.classList.remove('move-logo');
+            logo.style.animation = '';
+    
+            navLinks.forEach((link, index) => {
+                link.style.opacity = 1;
+                link.style.animation = '';
+            });
+        };
 
-    if (window.scrollY > 121.3125) {
-        nav.classList.add('move-nav');
-        logo.classList.add('move-logo');
-
-        navLinks.forEach((link, index) => {
-            link.style.opacity = 0;
-            link.style.animation = `linkFade 1s cubic-bezier(.11,.49,0,1.72) forwards ${index / 2 + .5}s`
-     });
-
-    } else if (window.scrollY < 121.3125) {
-        nav.classList.remove('move-nav');
-        logo.classList.remove('move-logo');
-
-        navLinks.forEach((link, index) => {
-            link.style.opacity = 1;
-            link.style.animation = '';
-        })
-    }
+    };
 };
 
 window.addEventListener('scroll', moveNav);
+
